a69f31314cb7126a906053970eadb303df1e0792
Simon-Kenn <simon.kenn.public@pm.me>
2024-09-18 15:48:33 +0200

    quantum-nvim initialisation

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Neovim derivation";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    gen-luarc.url = "github:mrcjkb/nix-gen-luarc-json";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-utils,
+    gen-luarc,
+    ...
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+
+    neovim-overlay = import ./nix/neovim-overlay.nix {inherit inputs;};
+  in
+    flake-utils.lib.eachSystem supportedSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          neovim-overlay
+          gen-luarc.overlays.default
+        ];
+      };
+      shell = pkgs.mkShell {
+        name = "nvim-devShell";
+        buildInputs = with pkgs; [
+          lua-language-server
+          nil
+          stylua
+          luajitPackages.luacheck
+        ];
+        shellHook = ''
+          # symlink the .luarc.json generated in the overlay
+          ln -fs ${pkgs.nvim-luarc-json} .luarc.json
+        '';
+      };
+    in {
+      packages = rec {
+        default = nvim;
+        nvim = pkgs.nvim-pkg;
+      };
+      devShells = {
+        default = shell;
+      };
+    })
+    // {
+      overlays.default = neovim-overlay;
+    };
+}
---
+++ nix/neovim-overlay.nix
@@ -0,0 +1,31 @@
+{inputs}: final: prev:
+with final.pkgs.lib; let
+  pkgs = final;
+
+  mkNvimPlugin = src: pname:
+    pkgs.vimUtils.buildVimPlugin {
+      inherit pname src;
+      version = src.lastModifiedDate;
+    };
+
+  pkgs-wrapNeovim = inputs.nixpkgs.legacyPackages.${pkgs.system};
+
+  mkNeovim = pkgs.callPackage ./mkNeovim.nix { inherit pkgs-wrapNeovim; };
+
+  all-plugins = with pkgs.vimPlugins; [
+  ];
+
+  extraPackages = with pkgs; [
+    lua-language-server
+    nil
+  ];
+in {
+  nvim-pkg = mkNeovim {
+    plugins = all-plugins;
+    inherit extraPackages;
+  };
+
+  nvim-luarc-json = final.mk-luarc-json {
+    plugins = all-plugins;
+  };
+}
