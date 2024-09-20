2cd1141a1990b91670694621771d1d6b0e8ffde9
Simon-Kenn <simon.kenn.public@pm.me>
2024-09-20 16:06:49 +0200

    Add treesitter syntax highliting

--- nvim/lua/modules/editor/configs.lua
+++ nvim/lua/modules/editor/configs.lua
@@ -1,2 +1,9 @@
 require('telescope').setup({})
 require('telescope').load_extension('fzf')
+
+require('nvim-treesitter.configs').setup({
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+})
