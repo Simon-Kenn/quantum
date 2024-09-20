19c7e36e0f8e0354045b91431841f28aebb433b1
Simon-Kenn <simon.kenn.public@pm.me>
2024-09-20 17:34:00 +0200

    add treesitter-textobjects

--- nvim/lua/modules/editor/configs.lua
+++ nvim/lua/modules/editor/configs.lua
@@ -1,5 +1,6 @@
 require('telescope').setup({}) -- TODO: Create shortcut and enhence configuration
 require('telescope').load_extension('fzf')
+-- TODO: add telescope tab extension
 
 require("nvim-autopairs").setup({})
 require("auto-save").setup({})
@@ -10,6 +11,7 @@ require('nvim-treesitter.configs').setup({
     enable = true,
     additional_vim_regex_highlighting = false,
   },
+
   incremental_selection = {
     enable = true,
     keymaps = {
@@ -19,4 +21,25 @@ require('nvim-treesitter.configs').setup({
       node_decremental = "<leader>id",
     },
   },
+
+  textobjects = {
+    select = {
+      enable = true,
+
+      lookahead = true,
+
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+      },
+      selection_modes = {
+        ['@parameter.outer'] = 'v', -- charwise
+        ['@function.outer'] = 'V', -- linewise
+        ['@class.outer'] = '<c-v>', -- blockwise
+      include_surrounding_whitespace = true,
+    },
+},
 })
