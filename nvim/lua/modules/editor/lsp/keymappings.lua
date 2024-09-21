b8cad26fa13f4295b95d90f5db6b4ab338e1aaad
Simon-Kenn <simon.kenn.public@pm.me>
2024-09-21 18:39:31 +0200

    Update keymaps

--- nvim/lua/modules/editor/lsp/keymappings.lua
+++ nvim/lua/modules/editor/lsp/keymappings.lua
@@ -2,16 +2,17 @@ function on_attach(client, bufnr)
   local map = vim.keymap.set
   local opts = { noremap = true, silent = true }
 
-  map('n', "grn", vim.lsp.buf.rename, opts, { desc = "Rename" })
-  map({'v', 'n'}, "gra", vim.lsp.buf.code_action, opts, { desc = "Code action" })
-  map('n', "grr", vim.lsp.buf.references, opts, { desc = "References" })
-  map('i', "<c-u>", vim.lsp.buf.signature_help, opts, { desc = "Signature help" })
+  map('n', 'R', vim.lsp.buf.hover, opts, { desc = 'Help/Hover' })
 
-  map('n', "R", vim.lsp.buf.hover, opts, { desc = "Help/Hover" })
+  map('n', 'gd', vim.lsp.buf.definition, opts, { desc = 'Go to definition' })
+  map('n', 'gD', vim.lsp.buf.declaration, opts, { desc = 'Go to declaration' })
+  map('n', 'gi', vim.lsp.buf.inmplementation, opts, { desc = 'Go to implementation' })
+  map('n', 'gr', vim.lsp.buf.references, opts, { desc = 'References' })
 
-  map('n', "gd", vim.lsp.buf.definition, opts, { desc = "Go to definition" })
-  map('n', "gD", vim.lsp.buf.implementation, opts, { desc = "Go to implementation" })
-  map('n', "g0", vim.lsp.buf.document_symbol, opts, { desc = "Document symbol" })
+  map('n', 'grn', vim.lsp.buf.rename, opts, { desc = 'Rename' })
+
+  map({ 'v', 'g.' }, 'gra', vim.lsp.buf.code_action, opts, { desc = 'Code action' })
+  map('i', '<c-u>', vim.lsp.buf.signature_help, opts, { desc = 'Signature help' })
 end
 
 return on_attach
