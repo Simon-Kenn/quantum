0b5c36a8e547efd4ee4e4df6c4e1a4aa779910b4
Simon-Kenn <simon.kenn.public@pm.me>
2024-09-21 15:14:55 +0200

    adding catppuccin plugins integration

--- nvim/lua/modules/ui/theme.lua
+++ nvim/lua/modules/ui/theme.lua
@@ -1,11 +1,38 @@
 require('catppuccin').setup({
-	flavour = "mocha",
-	integrations = {
-		which_key = true,
-		neogit = true,
-		gitsigns = true,
-		telescope = true,
-	},
+  flavour = "mocha",
+  integrations = {
+    which_key = true,
+    telescope = true,
+    treesitter = true,
+    treesitter_context = true,
+    rainbow_delimiters = true,
+    nvim_surround = true,
+    native_lsp = {
+      enabled = true,
+      virtual_text = {
+        errors = { "italic" },
+        hints = { "italic" },
+        warnings = { "italic" },
+        information = { "italic" },
+        ok = { "italic" },
+      },
+      underlines = {
+        errors = { "underline" },
+        hints = { "underline" },
+        warnings = { "underline" },
+        information = { "underline" },
+        ok = { "underline" },
+      },
+      inlay_hints = {
+        background = true,
+      },
+    },
+
+
+    diffview = true,
+    neogit = true,
+    gitsigns = true,
+  },
 })
 
 vim.cmd.colorscheme "catppuccin"
