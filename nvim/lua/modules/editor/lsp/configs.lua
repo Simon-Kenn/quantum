3985e29704178992a920b7a38865409d4b6417d3
Simon-Kenn <simon.kenn.public@pm.me>
2024-09-23 10:45:12 +0200

    load lazydev

--- nvim/lua/modules/editor/lsp/configs.lua
+++ nvim/lua/modules/editor/lsp/configs.lua
@@ -1,8 +1,10 @@
-require('lazydev')
 local lspconfig = require('lspconfig')
+local lazydev = require('lazydev')
 local on_attach = require('modules.editor.lsp.keymappings')
 
 lspconfig.rust_analyser.setup { on_attach = on_attach }
 lspconfig.nil_ls.setup { on_attach = on_attach }
 lspconfig.lua_ls.setup { on_attach = on_attach }
 lspconfig.clangd.setup { on_attach = on_attach }
+
+lazydev.setup() -- TODO: shoul be done only on lua file
