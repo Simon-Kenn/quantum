e3ced16ebcf065cdd6feddbe42a2e955fefc76b1
Simon-Kenn <simon.kenn.public@pm.me>
2024-09-24 16:38:16 +0200

    update autocompletion

--- nvim/lua/quantum/modules/editor/completion/init.lua
+++ nvim/lua/quantum/modules/editor/completion/init.lua
@@ -1,12 +1,12 @@
 local M = {}
-local cmp = require('cmp')
-local ls = require('luasnip')
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local utils = require('quantum.modules.editor.completion.utils')
-local kind_icons = require('quantum.modules.ui.utils').icons.kinds
+local ls, cmp = require("luasnip"), require("cmp")
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local utils, icons =
+  require("quantum.modules.editor.completion.utils"),
+  require("quantum.modules.ui.utils").icons.kinds
 
 function M.setup()
-  cmp.setup {
+  cmp.setup({
     snippet = {
       expand = function(args)
         ls.lsp_expand(args.body)
@@ -14,93 +14,79 @@ function M.setup()
     },
     window = {
       completion = {
-        border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
-        winhighlight = 'Normal:CmpPmenu,FloatBorder:CmpBorder,CursorLine:PmenuSel,Search:None',
+        border = "rounded",
+        winhighlight = "Normal:Pmenu,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
       },
       documentation = {
-        border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
-        winhighlight = 'Normal:CmpPmenu,FloatBorder:CmpBorder,CursorLine:PmenuSel,Search:None',
+        border = "rounded",
+        winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
       },
     },
     mapping = {
-      ['<CR>'] = cmp.mapping(function(fallback)
+      ["<CR>"] = cmp.mapping(function(fallback)
         utils.confirm_selection(fallback, ls, cmp)
       end),
-      ['<Tab>'] = cmp.mapping(function(fallback)
+      ["<Tab>"] = cmp.mapping(function(fallback)
         utils.next_item(fallback, ls, cmp)
-      end, { 'i', 's' }),
-      ['<S-Tab>'] = cmp.mapping(function(fallback)
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
         utils.prev_item(fallback, ls, cmp)
-      end, { 'i', 's' }),
-      ['<C-n>'] = cmp.mapping.select_next_item(),
-      ['<C-p>'] = cmp.mapping.select_prev_item(),
-      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-      ['<C-c>'] = cmp.mapping.abort(),
-      ['<C-g>'] = cmp.mapping.abort(),
+      end, { "i", "s" }),
+      ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+      ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+      ["<C-e>"] = cmp.mapping.abort(),
     },
     sources = cmp.config.sources({
       {
-        name = 'lazydev',
+        name = "lazydev",
         group_index = 0,
       },
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' },
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
     }, {
-      { name = 'path' },
-      { name = 'buffer' },
+      { name = "path" },
+      { name = "buffer" },
     }),
     formatting = {
-      fields = { 'menu', 'abbr', 'kind' },
-      format = function(entry, vim_item)
-        vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
-        vim_item.menu = ({
-          buffer = '[Buffer]',
-          nvim_lsp = '[LSP]',
-          luasnip = '[Luasnip]',
-          path = '[Path]',
-          lazydev = '[Lazydev]',
-          fonts = '[Fonts]',
-          emoji = '[Emoji]',
-          nerdfont = '[Nerdfont]',
-        })[entry.source.name]
-        return vim_item
+      expandable_indicator = false,
+      fields = { "kind", "abbr", "menu" },
+      format = function(entry, item)
+        if icons[item.kind] then
+          item.kind = icons[item.kind] .. item.kind
+        end
+        return item
       end,
     },
-  }
+  })
 
-  cmp.setup.filetype('nix', {
+  cmp.setup.filetype("nix", {
     sources = {
-      { name = 'fonts', options = { space_filter = '-' } },
+      { name = "fonts", options = { space_filter = "-" } },
     },
   })
 
-  cmp.setup.filetype('norg', {
+  cmp.setup.filetype("norg", {
     sources = {
-      { name = 'nerdfont' },
-      { name = 'emoji' },
+      { name = "nerdfont" },
+      { name = "emoji" },
     },
   })
 
-  cmp.setup.cmdline({ '/', '?' }, {
+  cmp.setup.cmdline({ "/", "?" }, {
     mapping = cmp.mapping.preset.cmdline(),
     sources = {
-      { name = 'buffer' },
+      { name = "buffer" },
     },
   })
 
-  cmp.setup.cmdline(':', {
+  cmp.setup.cmdline(":", {
     mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' },
-    }, {
-      { name = 'cmdline' },
-    }),
-    matching = { disallow_symbol_nonprefix_matching = false },
+    sources = {
+      { name = "cmdline" },
+    },
   })
 
-  cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+  cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 end
 
 return M
