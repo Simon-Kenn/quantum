2e971f73dc73ae47f138baa6dd34c13473cba6da
Simon-Kenn <simon.kenn.public@pm.me>
2024-09-24 15:29:51 +0200

    setup statuscol

--- nvim/lua/quantum/modules/ui/statuscol.lua
+++ nvim/lua/quantum/modules/ui/statuscol.lua
@@ -1,5 +1,25 @@
 local M = {}
 
-function M.setup() end
+function M.setup()
+  local builtin = require('statuscol.builtin')
+  local sign_name = { 'todo%-sign%-' }
+  require('statuscol').setup {
+    bt_ignore = { 'terminal', 'nofile' },
+    relculright = true,
+    segments = {
+      {
+        sign = {
+          name = sign_name,
+          namespace = { 'diagnostic/signs' },
+          maxwidth = 1,
+          auto = 1,
+        },
+      },
+      { text = { builtin.lnumfunc, ' ' } },
+      { sign = { namespace = { 'gitsign' }, maxwidth = 1, colwidth = 1, auto = true } },
+      { text = { builtin.foldfunc, ' ' } },
+    },
+  }
+end
 
 return M
