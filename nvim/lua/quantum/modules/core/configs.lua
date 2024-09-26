7c5ba0963bf8b2ea576f17b19f437f4fee5b3bde
Simon-Kenn <simon.kenn.public@pm.me>
2024-09-26 17:01:53 +0200

    add some which-key icons

--- nvim/lua/quantum/modules/core/configs.lua
+++ nvim/lua/quantum/modules/core/configs.lua
@@ -1,7 +1,39 @@
 local M = {}
 
 function M.setup()
-	require('which-key')
+  require("which-key").setup({
+    preset = "modern",
+    icons = {
+      rules = {
+        { pattern = "tag", icon = "", hl = "@string" },
+        { pattern = "todo", icon = "", hl = "@string" },
+        { pattern = "agenda", icon = "", hl = "@string" },
+        { pattern = "list", icon = "", hl = "@string" },
+        { pattern = "note", icon = "", hl = "@string" },
+        { pattern = "backlink", icon = "󰌹", hl = "@string" },
+        { pattern = "toc", icon = "󱏒", hl = "@string" },
+        { pattern = "date", icon = "󰃭", hl = "@string" },
+
+        { plugin = "oil.nvim", icon = "󰏇", hl = "@string" },
+        { pattern = "selecton", icon = "󰒉", hl = "@string" },
+        { pattern = "code", icon = "", hl = "@string" },
+        { pattern = "explorer", icon = "󰙅", hl = "@string" },
+        { pattern = "paste", icon = "", hl = "@string" },
+        { pattern = "yank", icon = "", hl = "@label" },
+        { pattern = "insert", icon = "", hl = "@string" },
+        { pattern = "save", icon = "󰆓", hl = "@number" },
+        { pattern = "buffer", icon = "", hl = "Structure" },
+        { pattern = "tab", icon = "", color = "red" },
+        { pattern = "grep", icon = "", color = "red" },
+        { pattern = "browser", icon = "", color = "red" },
+        { pattern = "help", icon = "", color = "red" },
+        { pattern = "buffers", icon = "", color = "red" },
+        { pattern = "view", icon = "", color = "red" },
+        { pattern = "error", icon = "", hl = "@number" },
+        { pattern = "quickfix", icon = "", hl = "Structure" },
+      },
+    },
+  })
 end
 
 return M
