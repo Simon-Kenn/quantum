e5e6c8aae78182ecdf38fa9f90e5e2758382ea3b
Simon-Kenn <simon.kenn.public@pm.me>
2024-09-26 18:11:23 +0200

    rename tabc

--- nvim/lua/quantum/modules/ui/keymappings.lua
+++ nvim/lua/quantum/modules/ui/keymappings.lua
@@ -1,8 +1,21 @@
 local M = {}
 local map = vim.keymap.set
 
+local function rename_tab()
+  vim.ui.input({ prompt = "Tab Name: " }, function(input)
+    if input ~= "" then
+      vim.api.nvim_command("TabRename " .. input)
+    else
+      print("No name provided")
+    end
+  end)
+end
+
 function M.setup()
   map("n", "<leader>j", "<cmd>Neotree toggle reveal<cr>", { desc = "Explorer" })
+  map("n", "<leader>mr", function()
+    rename_tab()
+  end, { desc = "rename tab" })
 end
 
 return M
